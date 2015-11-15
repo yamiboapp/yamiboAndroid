136d84c6ff65c55dbd393252624b42cf4f186c34
winfield <244892530@qq.com>
2015-11-15 16:40:43 +0800

    完整提交

---
+++ yamibo/src/main/java/com/yamibo/main/yamiboandroid/message/PublicMessageFragment.java
@@ -0,0 +1,24 @@
+package com.yamibo.main.yamiboandroid.message;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.yamibo.main.yamiboandroid.R;
+
+/**
+ * Created by WINFIELD on 2015/11/14.
+ */
+public class PublicMessageFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_message,null);
+
+    return view;
+    }
+
+
+
+}
---
+++ yamibo/src/main/java/com/yamibo/main/yamiboandroid/message/adapter/MessageListViewAdapter.java
@@ -0,0 +1,7 @@
+package com.yamibo.main.yamiboandroid.message.adapter;
+
+/**
+ * Created by WINFIELD on 2015/11/15.
+ */
+public class MessageListViewAdapter {
+}
