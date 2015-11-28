eb12db7db21728f653b78842c1f6901684c849ea
meki <371200212@qq.com>
2015-11-29 00:41:57 +0800

    burin uploaded 主要修改了抽屉和个人信息

--- yamibo/src/main/java/com/yamibo/main/yamiboandroid/main/MainActivity.java
+++ yamibo/src/main/java/com/yamibo/main/yamiboandroid/main/MainActivity.java
@@ -2,6 +2,7 @@ package com.yamibo.main.yamiboandroid.main;
 
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 
@@ -12,7 +13,7 @@ import com.yamibo.main.yamibolib.app.YMBActivity;
 public class MainActivity extends YMBActivity {
 
     private DrawerLayout mDrawerLayout;
-
+    private int drawer_state = 0;//0表示抽屉关闭，1表示抽屉打开
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +24,7 @@ public class MainActivity extends YMBActivity {
                 getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
         navigationDrawerFragment.setUp(R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
+
         getTitleBar().setLeftView(R.drawable.btn_main_menu, new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,5 +35,59 @@ public class MainActivity extends YMBActivity {
                 }
             }
         });
+
+        setdrawableListener();
+
+
+    }
+
+    /*监听抽屉的状态，根据状态显示或隐藏titlebar*/
+    public void setdrawableListener(){
+        mDrawerLayout.setDrawerListener(new DrawerLayout.DrawerListener() {
+            /**
+             * 当抽屉滑动状态改变的时候被调用
+             * 状态值是STATE_IDLE（闲置--0）, STATE_DRAGGING（拖拽的--1）, STATE_SETTLING（固定--2）中之一。
+             * 抽屉打开的时候，点击抽屉，drawer的状态就会变成STATE_DRAGGING，然后变成STATE_IDLE
+             */
+            @Override
+            public void onDrawerStateChanged(int arg0) {
+                Log.i("drawer", "drawer的状态：" + arg0);
+            }
+
+            /**
+             * 当抽屉被滑动的时候调用此方法
+             * arg1 表示 滑动的幅度（0-1）
+             */
+            @Override
+            public void onDrawerSlide(View arg0, float arg1) {
+                Log.i("drawer", arg1 + "");
+                    getTitleBar().hide();
+            }
+
+            /**
+             * 当一个抽屉被完全打开的时候被调用
+             */
+            @Override
+            public void onDrawerOpened(View arg0) {
+                Log.i("drawer", "抽屉被完全打开了！");
+                getTitleBar().hide();
+                drawer_state = 1;
+            }
+
+            /**
+             * 当一个抽屉完全关闭的时候调用此方法
+             */
+            @Override
+            public void onDrawerClosed(View arg0) {
+                Log.i("drawer", "抽屉被完全关闭了！");
+                getTitleBar().show();
+                drawer_state = 0;
+            }
+        });
+    }
+
+    protected void onResume() {
+        super.onResume();
+//        setdrawableListener();
     }
 }
